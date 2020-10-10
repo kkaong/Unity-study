545d45047f9e75b4a8a00e75b5657f6932cac408
Chae Won Shin <>
2020-10-10 17:04:38 +0900

    hihi

---
+++ Person.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+public class Person
+{
+    public string name;
+    public int age;
+    public string weapon;
+
+    public void print()
+    {
+        Console.WriteLine(age);
+        Console.WriteLine(name);
+        Console.WriteLine(weapon);
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace uniy
 {
@@ -6,7 +8,63 @@ namespace uniy
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Hello World!");
+            
+            int x = 3;
+
+            x = int.Parse(Console.ReadLine());
+            x = x+1;
+            x += 1;
+            
+
+            float strength = 15.5f;
+
+            string [] monsters = {"a", "b"};
+            int[] monsterLevel = new int[3];
+            monsterLevel[0] = 2;
+            monsterLevel[1] = 5;
+
+            Console.WriteLine("맵에 존재하는 몬스터의 레벨");
+            Console.WriteLine(monterLevel[0]);
+
+            List<string> items = new List<strings>();
+            items.Add("생명물약30");
+            items.Add("마나물약30");
+
+            items.RemoveAt(0);
+
+            Console.WriteLine("가지고 있는 아이템");
+            Console.WriteLine(items[0]);
+
+            Console.WriteLine("dddd"+"aaaa"+monsterLevel[0]);
+
+            int fullLevel = 99;
+            int level = 20;
+
+            //&& ||
+
+            //string condition = isBadCondition ? "나쁨" : "좋음" true 면 앞에, false 면 뒤에 출력 
+
+            for (int i = 0; i < monsters.Length; i++)
+                Console.WriteLine("이 지역 몬스터" + monsters[i]);
+            */
+            
+            Person a = new Person();
+            a.age = 23;
+            a.name = "Ho";
+            a.weapon = "gun";
+
+            a.print();
+
+
+
+
+            //Console.WriteLine(x);
+
         }
+
+        //pritvate static void 쓰면 아무것도 return안 해도 됨. 
+        //private static int calc(int x, int) 하면 뭔가 반환해야 함 
     }
 }
